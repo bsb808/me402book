944f03cedde0e1bfb792e8bfbc943f40df58c5ec
bsb <bsb@99bc8c20-0dbf-4db3-9f61-33371721be66>
2012-10-04 03:31:48 +0000

    Solutions for pre-lab

--- code/second_order_free_model.m
+++ code/second_order_free_model.m
@@ -1,21 +1,17 @@
 % Illustration of Second-Order LTI Free Response
 
 % Model Parameters 
-wn = 1*2*pi;  % undamped nat'l freq. [rad/s]
-zeta = 0.1;   % damping ratio [n/a]cd 
+wn = 32.6;  % undamped nat'l freq. [rad/s]
+zeta = 0.02;   % damping ratio [n/a]cd 
 
 % Initial condition
-y0 = 1;  %  y(t=0)
+y0 = 0.01;  %  y(t=0)
 v0 = 0;
 
 % Describe a vector of times for the solution
 dt = 0.001;   % Time step [s]
-Tend = 10;  % End of time horizon for simulation [s]
+Tend = 5;  % End of time horizon for simulation [s]
 tt = 0:dt:Tend;  
-
-% Mistaken Solution to DE
-wd = wn*sqrt(1-zeta^2);
-Yeqn = y0*exp(-zeta*wn*tt).*cos(wd*tt);
 
 % Solution to DE
 wd = wn*sqrt(1-zeta^2);
@@ -29,6 +25,5 @@ figure(1); clf()
 %hold on
 plot(tt,Yeqn2,'r-')
 xlabel('Time [sec]')
-ylabel('y(t) [n/a]')
-legend('Mistaken','Corrected')
-title('Free Response of Second-Order Model')
+ylabel('y(t) [cm]')
+title(sprintf('Free Response: wn=%5.2f rad/s, f_n=%5.2f zeta=%5.3f',wn,wn/(2*pi),zeta))
