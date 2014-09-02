48e39b904c7079e76a3ba9aa0a7a74c4f17084df
bsb <bsb@99bc8c20-0dbf-4db3-9f61-33371721be66>
2014-09-02 19:28:11 +0000

    adding code

---
+++ code/firstorder_car_cruise.m
@@ -0,0 +1,30 @@
+% Illustration of First-Order LTI Step Response
+
+% Clear all the variables in the workspace
+clear;
+
+% System Parameters 
+m = 800;       % mass [kg]
+b = 225;      % linear drag [Ns/m]
+Fstep = 15000;  % amplitude of step force input [N]
+
+% Describe a vector of times for the simulation
+dt = 0.1;   % Time step [s]
+Tend = 15;  % End of time horizon for simulation [s]
+tt = 0:dt:Tend;  
+
+% Analytical Solution
+tau = m/b;
+% Note that MATLAB can deal with the tt vector in this equation
+% and output a vector of velcocities.
+Veqn = Fstep/b*(1-exp(-tt/tau));  
+
+% Plot the results
+figure(1)
+clf()
+plot(tt,Veqn/0.447,'b.')
+xlabel('Time [sec]')  % Always label the axes and include units!
+ylabel('Velocity [m/s]')
+title('First-Order Step Response')
+grid on
+
